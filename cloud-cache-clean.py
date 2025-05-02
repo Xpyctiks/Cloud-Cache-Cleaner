ef0f0da038bb7f90b408922dfcc49776aba05f60
Ruslan Sieriebriannikov <xpyct@xpyct.com>
2025-05-02 11:05:09 +0300

    Before addon of local cache purge

--- cloud-cache-clean.py
+++ cloud-cache-clean.py
@@ -13,9 +13,9 @@ from cryptography.fernet import Fernet
 CONFIG_FILE = os.path.abspath(os.path.dirname(__file__))+"/cloud-cache-clean.conf"
 PASSWORD_FILE = os.path.abspath(os.path.dirname(__file__))+"/user-pass.conf"
 #salt for encrypt cookie
-COOKIE_SALT = "NFc5834073Df2d20AA19c9Nm05YK98cf"
+COOKIE_SALT = ""
 #key to ecnrypt API token in <hidden> filed to easy process of purge method
-CRYPT_KEY = b"TkZjNTgzNDA3M0RmMmQyMEFBMTljOU5tMDVZSzk4Y2Y="
+CRYPT_KEY = b""
 PWD_LIST = []
 TELEGRAM_TOKEN = ""
 TELEGRAM_CHATID = ""
@@ -41,9 +41,13 @@ def load_config():
         global TELEGRAM_CHATID
         global LOG_FILE
         global CF_ACCOUNTS
+        global COOKIE_SALT
+        global CRYPT_KEY
         TELEGRAM_TOKEN = config.get('telegramToken')
         TELEGRAM_CHATID = config.get('telegramChat')
         LOG_FILE = config.get('logFile')
+        COOKIE_SALT = config.get('cookie_salt')
+        CRYPT_KEY = config.get('crypt_key')
         CF_ACCOUNTS = config.get('CFaccounts', [])
         logging.basicConfig(filename=LOG_FILE,level=logging.INFO,format='%(asctime)s - Cloud-cache-clean - %(levelname)s - %(message)s',datefmt='%d-%m-%Y %H:%M:%S')
     else:
@@ -82,13 +86,13 @@ def generate_default_config2():
     config2 = {
         "admin": {
             "Password": "",
-            "Name": "Account1",
+            "Name": "Administrator",
             "Permissions": "*"
         },
         "user": {
             "Password": "",
-            "Name": "Account2",
-            "Permissions": "Account1"
+            "Name": "User",
+            "Permissions": "Account1,Account2"
         }
     }
     with open(PASSWORD_FILE, 'w',encoding='utf8') as file:
@@ -132,10 +136,14 @@ def purge():
         url = f"https://api.cloudflare.com/client/v4/zones/{request.form['zoneid']}/purge_cache"
         response = requests.post(url, json={"purge_everything": True}, headers=headers)
         if response.status_code == 200:
+            send_to_telegram("🍀Cloud-Cache-Clean:",f"CloudFlare cache for {request.form['purge']} purged successfully by {request.cookies.get('realname')}!")
+            logging.info(f"CloudFlare cache for {request.form['purge']} purged successfully by {request.cookies.get('realname')}!")
             response = make_response(redirect("/"),301)
             response.set_cookie("result", f"Purged successfully!", max_age=5)
             return response
         else:
+            send_to_telegram("💢Cloud-Cache-Clean:",f"Error puring CloudFlare cache for {request.form['purge']} by {request.cookies.get('realname')}!")
+            logging.error(f"Error puring CloudFlare cache for {request.form['purge']} by {request.cookies.get('realname')}!")
             response = make_response(redirect("/"),301)
             response.set_cookie("result", f"Some error during purge operation!", max_age=5)
             return response
@@ -171,14 +179,16 @@ def login():
                 <button type=\"button\" class=\"btn-close\" data-bs-dismiss=\"alert\" aria-label=\"Close\"></button>
                 </div>"""
                 logging.error(f"Login: Wrong password \"{request.form['password']}\" for user \"{request.form['username']}\"")
+                send_to_telegram("🚷Cloud-Cache-Clean:",f"Login error.Wrong password for user \"{request.form['username']}\"")
                 return render_template("template-login.html",loginError=loginError)
         #if user is not found - show error message.Adding error message to the login form
         else:
             loginError = f"""<div class=\"alert alert-danger alert-dismissible fade show\" role=\"alert\" style=\"margin-top: 15px;\">
-            Wrong username or password!"
+            Wrong username or password!
             <button type=\"button\" class=\"btn-close\" data-bs-dismiss=\"alert\" aria-label=\"Close\"></button>
             </div>"""
             logging.error(f"Login: Unknown user \"{request.form['username']}\", password \"{request.form['password']}\"")
+            send_to_telegram("🚷Cloud-Cache-Clean:",f"Login error: Unknown user \"{request.form['username']}\" login attempt!")
             return render_template("template-login.html",loginError=loginError)
     #if this is GET request - just show login form
     if request.method == 'GET':
@@ -186,11 +196,12 @@ def login():
     
 @application.route("/", methods=['GET'])
 def index():
+    load_config()
     if request.cookies.get("SESSID") and request.cookies.get("realname") and request.cookies.get("username"):
         #searching for the given user in the list
         for id,user in enumerate(PWD_LIST.keys()):
             dataLocal = f"{COOKIE_SALT}{request.cookies.get('username')}".encode('utf-8')
-            dataReceived = request.cookies.get("SESSID").encode('utf-8')
+            dataReceived = request.cookies.get('SESSID').encode('utf-8')
             #checking if user is found and encrypted cookie he sent is correct
             if bcrypt.checkpw(dataLocal,dataReceived):
                 res = request.cookies.get("result")
@@ -206,54 +217,65 @@ def index():
         return response
 
 def index2(realname,result):
-    table = ""
-    id = 1
-    #getting all permissions from current user as the list
-    permissions_list = [item.strip() for item in PWD_LIST[request.cookies.get("username")]['Permissions'].split(',')]
-    for account in CF_ACCOUNTS:
-        print(f"Checking {account['Name']} in {permissions_list}")
-        if account['Name'] in permissions_list or "*" in permissions_list:
-            headers = {
-                'Authorization': f"Bearer {account['Token']}",
-                'Content-Type':  'application/json'
-            }
-            cipher = Fernet(CRYPT_KEY)
-            url = 'https://api.cloudflare.com/client/v4/zones'
-            hash = cipher.encrypt(account['Token'].encode('utf-8')).decode('utf-8')
-            response = requests.get(url, headers=headers)    
-            if response.status_code == 200: 
-                for i in response.json()["result"]:
-                    if i['status'] != "active":
-                        table += f"""\n<tr>\n<th scope="row" class="table-danger">{id}</th>
-                        <td class="table-danger"><form method="post" action="/purge"><button type="submit" id="purge" name="purge" class="btn btn-primary">Purge Cache</button>
-                        <input type="hidden" name="zoneid" value="{i['id']}">
-                        <input type="hidden" name="hash" value="{hash}"></form></td>
-                        <td class="table-danger">{i['name']}</td>
-                        <td class="table-danger">{i['status']}</td>
-                        <td class="table-danger">{i['name_servers'][0]}, {i['name_servers'][1]}</td>
-                        <td class="table-danger">{i['account']['name']}</td>
-                        <td class="table-danger">{i['id']}</td>
-                        <td class="table-danger">{i['original_registrar']}</td>
-                        <td class="table-danger">{i['plan']['name']}</td>\n</tr>"""
-                    else:
-                        table += f"""\n<tr>\n<th scope="row" class="table-success">{id}</th>
-                        <td class="table-success"><form method="post" action="/purge"><button type="submit" id="purge" name="purge" class="btn btn-primary">Purge Cache</button>
-                        <input type="hidden" name="zoneid" value="{i['id']}">
-                        <input type="hidden" name="hash" value="{hash}"></form></td>
-                        <td class="table-success">{i['name']}</td>
-                        <td class="table-success">{i['status']}</td>
-                        <td class="table-success">{i['name_servers'][0]}, {i['name_servers'][1]}</td>
-                        <td class="table-success">{i['account']['name']}</td>
-                        <td class="table-success">{i['id']}</td>
-                        <td class="table-success">{i['original_registrar']}</td>
-                        <td class="table-success">{i['plan']['name']}</td>\n</tr>"""
-                    id += 1
-            else:
-                print(f"Error:{response}")
-    if result == "":
-        return render_template("template-main.html",table=table,realName=realname)
-    else:
-        return render_template("template-main.html",table=table,realName=realname,result=result)
+    try:
+        table = ""
+        id = 1
+        nameserver1 = ""
+        nameserver2 = ""
+        #getting all permissions from current user as the list
+        permissions_list = [item.strip() for item in PWD_LIST[request.cookies.get("username")]['Permissions'].split(',')]
+        for account in CF_ACCOUNTS:
+            if account['Name'] in permissions_list or "*" in permissions_list:
+                headers = {
+                    'Authorization': f"Bearer {account['Token']}",
+                    'Content-Type':  'application/json'
+                }
+                cipher = Fernet(CRYPT_KEY)
+                url = 'https://api.cloudflare.com/client/v4/zones'
+                hash = cipher.encrypt(account['Token'].encode('utf-8')).decode('utf-8')
+                response = requests.get(url, headers=headers) 
+                if response.status_code == 200:
+                    for i in response.json()["result"]:
+                        if not 'name_servers' in i:
+                            nameserver1 = "None"
+                            nameserver2 = "None"
+                        else:
+                            nameserver1 = i['name_servers'][0]
+                            nameserver2 = i['name_servers'][1]
+                        if i['status'] != "active":
+                            table += f"""\n<tr>\n<th scope="row" class="table-danger">{id}</th>
+                            <td class="table-danger"><form method="post" action="/purge"><button type="submit" value="{i['name']}" name="purge" class="btn btn-primary">Purge Cache</button>
+                            <input type="hidden" name="zoneid" value="{i['id']}">
+                            <input type="hidden" name="hash" value="{hash}"></form></td>
+                            <td class="table-danger">{i['name']}</td>
+                            <td class="table-danger">{i['status']}</td>
+                            <td class="table-danger">{nameserver1}, {nameserver2}</td>
+                            <td class="table-danger">{i['account']['name']}</td>
+                            <td class="table-danger">{i['id']}</td>
+                            <td class="table-danger">{i['original_registrar']}</td>
+                            <td class="table-danger">{i['plan']['name']}</td>\n</tr>"""
+                        else:
+                            table += f"""\n<tr>\n<th scope="row" class="table-success">{id}</th>
+                            <td class="table-success"><form method="post" action="/purge"><button type="submit" value="{i['name']}" name="purge" class="btn btn-primary">Purge Cache</button>
+                            <input type="hidden" name="zoneid" value="{i['id']}">
+                            <input type="hidden" name="hash" value="{hash}"></form></td>
+                            <td class="table-success">{i['name']}</td>
+                            <td class="table-success">{i['status']}</td>
+                            <td class="table-success">{nameserver1}, {nameserver2}</td>
+                            <td class="table-success">{i['account']['name']}</td>
+                            <td class="table-success">{i['id']}</td>
+                            <td class="table-success">{i['original_registrar']}</td>
+                            <td class="table-success">{i['plan']['name']}</td>\n</tr>"""
+                        id += 1
+                else:
+                    print(f"Error:{response}")
+                    logging.error(f"Error:{response}")
+        if result == "":
+            return render_template("template-main.html",table=table,realName=realname)
+        else:
+            return render_template("template-main.html",table=table,realName=realname,result=result)
+    except Exception as msg:
+        logging.error(f"Error in index2(): {msg}")
 
 def genpwd():
     if len(sys.argv) < 3:
@@ -279,11 +301,7 @@ if __name__ == "__main__":
         #if we are generating the password hash
         elif sys.argv[1] == "genpwd":
             genpwd()
-        #reload functions to apply changes from conf. files
-        elif sys.argv[1] == "reload":
-            load_config()
         else:
             print(f"Something went wrong. Please check the parameters.")
             quit()
     load_config()
-    application.run(host='0.0.0.0',port=8500,debug=False)
